78662f473a3b927fac5c05d5c61bb57a06d32820
Micaela Del Longo <micaeladellongo@gmail.com>
2023-11-12 20:06:24 -0300

    Final update

--- code/runner.R
+++ code/runner.R
@@ -1,7 +1,6 @@
-# library(caret)
-# library(randomForest)
-# library(dplyr)
-# library(ROSE)
+library(randomForest)
+library(dplyr)
+library(ROSE)
 
 # train_data <- read.csv("data/data-parte-b/arbolado-mza-dataset.csv")
 # train_data$ultima_modificacion <- NULL
@@ -67,44 +66,49 @@
 # # guardar con feature engineering
 # write.csv(train_data, "data/data-parte-b/train-data-FE.csv", row.names = FALSE)
 # write.csv(test_data, "data/data-parte-b/test-data-FE.csv", row.names = FALSE)
+base_filename <- "data/submissions/submission_"
+index_filename <- 59
 
-train_data <- read.csv("data/data-parte-b/train-data-FE.csv")
-test_data <- read.csv("data/data-parte-b/test-data-FE.csv")
+for (i in 1:10) {
+    train_data <- read.csv("data/data-parte-b/train-data-FE.csv")
+    test_data <- read.csv("data/data-parte-b/test-data-FE.csv")
 
-# balanceo de clases ---------------------------------------------------------------------------------------------------
-train_data <- ovun.sample(inclinacion_peligrosa ~ ., data = train_data, method = "under")$data  # default p = 0.5
+    # balanceo de clases ---------------------------------------------------------------------------------------------------
+    train_data <- ovun.sample(inclinacion_peligrosa ~ ., data = train_data, method = "under")$data  # default p = 0.5
 
-# class_distribution <- table(train_data$inclinacion_peligrosa)
-# class_probability <- class_distribution / sum(class_distribution)
-# class_weights <- class_distribution[2] / class_distribution
+    # class_distribution <- table(train_data$inclinacion_peligrosa)
+    # class_probability <- class_distribution / sum(class_distribution)
+    # class_weights <- class_distribution[2] / class_distribution
 
-# random forest --------------------------------------------------------------------------------------------------------
+    # random forest --------------------------------------------------------------------------------------------------------
 
-# pasar a factor para hacerlo de clasificación
-# train_data$inclinacion_peligrosa <- factor(train_data$inclinacion_peligrosa)
+    # pasar a factor para hacerlo de clasificación
+    # train_data$inclinacion_peligrosa <- factor(train_data$inclinacion_peligrosa)
 
-formula <- inclinacion_peligrosa ~ circ_tronco_cm + lat + long + peligrosidad_especie + peligrosidad_diametro + peligrosidad_altura
-rf_model <- randomForest(formula,
-                         data = train_data,
-                         do.trace = TRUE,
-                         mtry = 3,  # Number of variables randomly sampled at each split. Default values: for classification sqrt(p) and regression p/3
-                         # classwt = class_probability,
-                         importance = TRUE,
-                         proximity = TRUE,
-                         ntree = 600
-)
+    formula <- inclinacion_peligrosa ~ circ_tronco_cm + lat + long + peligrosidad_especie + peligrosidad_diametro + peligrosidad_altura
+    rf_model <- randomForest(formula,
+                             data = train_data,
+                             do.trace = TRUE,
+                             mtry = 3,  # Number of variables randomly sampled at each split. Default values: for classification sqrt(p) and regression p/3
+                             # classwt = class_probability,
+                             importance = TRUE,
+                             proximity = TRUE,
+                             ntree = 600
+    )
 
-predictions <- predict(rf_model, test_data)
-predictions_numeric <- as.numeric(as.character(predictions)) # predictions es un factor, hay que pasarlo a numeric
+    predictions <- predict(rf_model, test_data)
+    predictions_numeric <- as.numeric(as.character(predictions)) # predictions es un factor, hay que pasarlo a numeric
 
-result_dataset <- data.frame(id = test_data$id, inclinacion_peligrosa = predictions_numeric)
-write.csv(result_dataset, "data/submissions/submission_48.csv", row.names = FALSE)
-
-# comparación con el mejor valor
-# print("Comparación con el mejor valor")
-# sub <- read.csv("data/submissions/submission_38.csv")
-# percent <- sum(sub$inclinacion_peligrosa == 1) / nrow(sub) * 100
-# print(percent)
-#
-# percent_prediction <- sum(predictions_numeric == 1) / length(predictions_numeric) * 100
-# print(percent_prediction)
+    result_dataset <- data.frame(id = test_data$id, inclinacion_peligrosa = predictions_numeric)
+    full_filename <- paste0(base_filename, index_filename, ".csv")
+    write.csv(result_dataset, full_filename, row.names = FALSE)
+    index_filename <- index_filename + 1
+    # comparación con el mejor valor
+    # print("Comparación con el mejor valor")
+    # sub <- read.csv("data/submissions/submission_38.csv")
+    # percent <- sum(sub$inclinacion_peligrosa == 1) / nrow(sub) * 100
+    # print(percent)
+    #
+    # percent_prediction <- sum(predictions_numeric == 1) / length(predictions_numeric) * 100
+    # print(percent_prediction)
+}
